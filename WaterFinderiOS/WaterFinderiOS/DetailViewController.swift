91dc1a7c60ccc007276d9d31c5b2d4341704e682
Alexis Saint-Jean <asaintjean@gmail.com>
2015-04-30 03:00:50 +0200

    Recreated OptionsVC and DetailVC

---
+++ WaterFinderiOS/WaterFinderiOS/DetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DetailViewController.swift
+//  WaterFinderiOS
+//
+//  Created by Alexis Saint-Jean on 4/30/15.
+//  Copyright (c) 2015 Alexis Saint-Jean. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBAction func closingButton(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
